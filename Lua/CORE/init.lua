31688079a37aaddd6102c815584c7da3e6984caa
Dimitri Barronmore <dimitribarronmore@pm.me>
2021-04-12 15:42:35 -0600

    altered require statements to find the current folder

--- Lua/CORE/init.lua
+++ Lua/CORE/init.lua
@@ -1,4 +1,4 @@
- 
+local current_folder = (...):gsub('%init$', '')
 --[[
 	
 	This file automatically initializes all major CORE libraries.
@@ -8,12 +8,12 @@
 --]]
 
 -- just about everything relies on this in one way or another
-require "CORE/Setup/changed_functions"
+require (current_folder .. "Setup/changed_functions")
 
 -- big libraries, in necessary order
-require "CORE/Events"
-require "CORE/Setup/initialize_events"
-require "CORE/Overwrite"
+require (current_folder .. "Events")
+require (current_folder .. "Setup/initialize_events")
+require (current_folder .. "Overwrite")
 
 -- script-specific libraries, just in case
 local enc = SetButtonLayer
@@ -21,8 +21,8 @@ local mons = Kill
 local wave = EndWave
 
 if (not (mons or wave)) or enc then -- Encounter-only
-	require "CORE/Inheritance"
-	require "CORE/Setup/initialize_wave_sandbox"
+	require (current_folder .."Inheritance")
+	require (current_folder .. "Setup/initialize_wave_sandbox")
 
 end
 
